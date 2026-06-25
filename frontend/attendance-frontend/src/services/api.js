5e52760200778c18aa1ecb03bad1f25a37ee89cb
akashdevkatoch <mrakashthakur2@gmail.com>
2026-06-25 16:27:43 +0530

    Production API URL

--- frontend/attendance-frontend/src/services/api.js
+++ frontend/attendance-frontend/src/services/api.js
@@ -2,52 +2,35 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://127.0.0.1:5000/api",
+    "https://attendance-backend-cptr.onrender.com/api",
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token =
-      localStorage.getItem(
-        "token"
-      );
+    const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   },
-  (error) => {
-    return Promise.reject(
-      error
-    );
-  }
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
   (response) => response,
 
   (error) => {
-    if (
-      error.response?.status ===
-      401
-    ) {
-      alert(
-        "Session Expired. Please Login Again."
-      );
+    if (error.response?.status === 401) {
+      alert("Session Expired. Please Login Again.");
 
       localStorage.clear();
 
-      window.location.replace(
-        "/"
-      );
+      window.location.replace("/");
     }
 
-    return Promise.reject(
-      error
-    );
+    return Promise.reject(error);
   }
 );
 
